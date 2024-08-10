25679b5fb40445e1a617d003f4065dec2ba6c9cb
Kang Seung Min <empe8339@gmail.com>
2024-08-10 22:32:38 +0900

    Resolve encoding inverse_transform

--- model/pipeline/data_preparation.py
+++ model/pipeline/data_preparation.py
@@ -1,9 +1,9 @@
+data_preparation.py
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn import model_selection
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
+
 
 class DataPrep(object):
     
@@ -22,8 +22,11 @@ class DataPrep(object):
     Methods:
     1) __init__() -> instantiates DataPrep object and handles the pre-processing steps for feeding it to the training algorithm
     2) inverse_prep() -> deals with post-processing of the generated data to have the same format as the original dataset
+
+
     """
-    
+
+
     def __init__(self, raw_df: pd.DataFrame, categorical: list, log:list, mixed:dict, integer:list, type:dict, test_ratio:float):
         
         self.categorical_columns = categorical
@@ -34,109 +37,103 @@ class DataPrep(object):
         self.column_types["categorical"] = []
         self.column_types["mixed"] = {}
         self.lower_bounds = {}
-        self.ordinal_encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
-        self.label_encoder = LabelEncoder()
-        self.imputer = SimpleImputer(strategy='mean')
+        self.label_encoder_list = []
 
-        # 타겟 열 이름 가져오기
-        self.target_col = list(type.values())[0]
-        self.target_categories = raw_df[self.target_col].unique()
         # Spliting the input data to obtain training dataset
-        y_real = raw_df[self.target_col]
-        X_real = raw_df.drop(columns=[self.target_col])
-        X_train_real, _, y_train_real, _ = model_selection.train_test_split(X_real, y_real, test_size=test_ratio, stratify=y_real, random_state=42)        
-        self.df = X_train_real
-        self.df[self.target_col] = y_train_real
+        target_col = list(type.values())[0]
+        y_real = raw_df[target_col]
+        X_real = raw_df.drop(columns=[target_col])
+        X_train_real, _, y_train_real, _ = model_selection.train_test_split(X_real ,y_real, test_size=test_ratio, stratify=y_real,random_state=42)        
+        X_train_real[target_col]= y_train_real
 
         # Replacing empty strings with na if any and replace na with empty
+        self.df = X_train_real
         self.df = self.df.replace(r' ', np.nan)
         self.df = self.df.fillna('empty')
         
         # Dealing with empty values in numeric columns by replacing it with -9999999 and treating it as categorical mode 
-        all_columns = set(self.df.columns)
-        categorical_columns = self.categorical_columns
-        irrelevant_missing_columns = set(categorical_columns)
+        all_columns= set(self.df.columns)
+        irrelevant_missing_columns = set(self.categorical_columns)
         relevant_missing_columns = list(all_columns - irrelevant_missing_columns)
-        #
+        
         for i in relevant_missing_columns:
             if i in list(self.mixed_columns.keys()):
                 if "empty" in list(self.df[i].values):
-                    self.df[i] = self.df[i].apply(lambda x: -9999999 if x == "empty" else x)
+                    self.df[i] = self.df[i].apply(lambda x: -9999999 if x=="empty" else x )
                     self.mixed_columns[i].append(-9999999)
             else:
                 if "empty" in list(self.df[i].values):   
-                    self.df[i] = self.df[i].apply(lambda x: -9999999 if x == "empty" else x)
+                    self.df[i] = self.df[i].apply(lambda x: -9999999 if x=="empty" else x)
                     self.mixed_columns[i] = [-9999999]
         
         # Dealing with skewed exponential numeric distributions by applying log transformation
         if self.log_columns:
             for log_column in self.log_columns:
+                # Value added to apply log to non-positive numeric values
                 eps = 1 
-                lower = np.min(self.df.loc[self.df[log_column] != -9999999][log_column].values) 
+                # Missing values indicated with -9999999 are skipped
+                lower = np.min(self.df.loc[self.df[log_column]!=-9999999][log_column].values) 
                 self.lower_bounds[log_column] = lower
-                if lower > 0: 
-                    self.df[log_column] = self.df[log_column].apply(lambda x: np.log(x) if x != -9999999 else -9999999)
+                if lower>0: 
+                    self.df[log_column] = self.df[log_column].apply(lambda x: np.log(x) if x!=-9999999 else -9999999)
                 elif lower == 0:
-                    self.df[log_column] = self.df[log_column].apply(lambda x: np.log(x + eps) if x != -9999999 else -9999999) 
+                    self.df[log_column] = self.df[log_column].apply(lambda x: np.log(x+eps) if x!=-9999999 else -9999999) 
                 else:
-                    self.df[log_column] = self.df[log_column].apply(lambda x: np.log(x - lower + eps) if x != -9999999 else -9999999)
+                    # Negative values are scaled to become positive to apply log
+                    self.df[log_column] = self.df[log_column].apply(lambda x: np.log(x-lower+eps) if x!=-9999999 else -9999999)
         
-        # Applying OrdinalEncoding to all columns except the target column
-        columns_to_encode = [col for col in self.df.columns if col != self.target_col]
-        self.df[columns_to_encode] = pd.DataFrame(self.ordinal_encoder.fit_transform(self.df[columns_to_encode]), columns=columns_to_encode)
-
-        # 타겟 열에 LabelEncoding 적용
-        self.df[self.target_col] = self.label_encoder.fit_transform(self.df[self.target_col])
-
-        # Storing feature order
-        self.feature_order = self.df.columns.tolist()
+        # Encoding categorical column using label encoding to assign each category within a column with an integer value
+        for column_index, column in enumerate(self.df.columns):
+            
+            if column in self.categorical_columns:        
+                label_encoder = preprocessing.LabelEncoder()
+                self.df[column] = self.df[column].astype(str)
+                label_encoder.fit(self.df[column])
+                current_label_encoder = dict()
+                current_label_encoder['column'] = column
+                current_label_encoder['label_encoder'] = label_encoder
+                transformed_column = label_encoder.transform(self.df[column])
+                self.df[column] = transformed_column
+                self.label_encoder_list.append(current_label_encoder)
+                self.column_types["categorical"].append(column_index)
+            
+            elif column in self.mixed_columns:
+                self.column_types["mixed"][column_index] = self.mixed_columns[column]
         
         super().__init__()
-    
-    def inverse_prep(self, data, eps=1):
-        # Converting generated data into a dataframe and assign column names as per original dataset
-        df_sample = pd.DataFrame(data, columns=self.df.columns)
-
-        target_col = 'Fraud_Type'
-
-        # Reversing the label encoding assigned to target column (LabelEncoder)
-        if target_col in df_sample.columns:
-            df_sample[target_col] = np.round(df_sample[target_col]).astype(int)
-            df_sample[target_col] = df_sample[target_col].map(lambda x: 0 if x < 0 else x)
-            
-            # Ensure that all values are within the valid range of the label encoder
-            max_label_index = len(self.label_encoder.classes_) - 1
-            df_sample[target_col] = df_sample[target_col].map(lambda x: max_label_index if x > max_label_index else x)
-            
-            df_sample[target_col] = self.label_encoder.inverse_transform(df_sample[target_col])
         
-        # Reversing log transformation by applying exponential transformation with appropriate scaling for non-positive numeric columns
+    def inverse_prep(self, data, eps=1):
+        
+        # Converting generated data into a dataframe and assign column names as per original dataset
+        df_sample = pd.DataFrame(data,columns=self.df.columns)
+        
+        # Reversing the label encoding assigned to categorical columns according to the original dataset 
+        for i in range(len(self.label_encoder_list)):
+            le = self.label_encoder_list[i]["label_encoder"]
+            df_sample[self.label_encoder_list[i]["column"]] = df_sample[self.label_encoder_list[i]["column"]].astype(int)
+            df_sample[self.label_encoder_list[i]["column"]] = le.inverse_transform(df_sample[self.label_encoder_list[i]["column"]])
+
+        # Reversing log by applying exponential transformation with appropriate scaling for non-positive numeric columns 
+        # -9999999 used to denote missing values are similarly ignored
         if self.log_columns:
-            for column in self.log_columns:
-                if column in df_sample.columns:
-                    lower_bound = self.lower_bounds[column]
-                    if lower_bound > 0:
-                        df_sample[column] = df_sample[column].apply(lambda x: np.exp(x) if x != -9999999 else -9999999)
-                    elif lower_bound == 0:
-                        df_sample[column] = df_sample[column].apply(lambda x: np.ceil(np.exp(x) - eps) if ((x != -9999999) & ((np.exp(x) - eps) < 0)) else (np.exp(x) - eps if x != -9999999 else -9999999))
-                    else:
-                        df_sample[column] = df_sample[column].apply(lambda x: np.exp(x) - eps + lower_bound if x != -9999999 else -9999999)
+            for i in df_sample:
+                if i in self.log_columns:
+                    lower_bound = self.lower_bounds[i]
+                    if lower_bound>0:
+                        df_sample[i].apply(lambda x: np.exp(x) if x!=-9999999 else -9999999) 
+                    elif lower_bound==0:
+                        df_sample[i] = df_sample[i].apply(lambda x: np.ceil(np.exp(x)-eps) if ((x!=-9999999) & ((np.exp(x)-eps) < 0)) else (np.exp(x)-eps if x!=-9999999 else -9999999))
+                    else: 
+                        df_sample[i] = df_sample[i].apply(lambda x: np.exp(x)-eps+lower_bound if x!=-9999999 else -9999999)
         
         # Rounding numeric columns without floating numbers in the original dataset
         if self.integer_columns:
             for column in self.integer_columns:
-                if column in df_sample.columns:
-                    df_sample[column] = np.round(df_sample[column].values)
-                    df_sample[column] = df_sample[column].astype(int)
+                df_sample[column]= (np.round(df_sample[column].values))
+                df_sample[column] = df_sample[column].astype(int)
 
         # Converting back -9999999 and "empty" to na
-        df_sample.replace(-9999999, np.nan, inplace=True)
-        df_sample.replace('empty', np.nan, inplace=True)
-
-        # Reversing the ordinal encoding for all columns except the target column
-        columns_to_decode = [col for col in self.df.columns if col != self.target_col]
-        df_sample[columns_to_decode] = pd.DataFrame(self.ordinal_encoder.inverse_transform(df_sample[columns_to_decode]), columns=columns_to_decode)
+        df_sample.replace(-9999999, np.nan,inplace=True)
+        df_sample.replace('empty', np.nan,inplace=True)
 
         return df_sample
-    
-    ##target은 label, 다른 컬럼들은 Ordinal을 사용했지만, inverse가 정상적으로 돌아가지 않는다. 아마 feature를 제대로 읽지 못하는 느낌..
